8aa4ecaf57285b264361f89e3f6c8dfcc56ff585
Mikhail <muzhikovm@bk.ru>
2019-02-10 15:02:39 +0300

    added attribute 'controls'

--- №3/script.js
+++ №3/script.js
@@ -10,10 +10,8 @@ function changeTextLang(){
     let ruText = ["Повседневная практика показывает, что постоянное информационно-пропагандистское обеспечение нашей деятельности способствует подготовки и реализации направлений прогрессивного развития.", "Повседневная практика показывает, что рамки и место обучения кадров в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям.", "Не следует, однако забывать, что начало повседневной работы по формированию позиции способствует подготовки и реализации систем массового участия.", "Разнообразный и богатый опыт начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки позиций, занимаемых участниками в отношении поставленных задач.", "Разнообразный и богатый опыт сложившаяся структура организации требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач."]
 
     switch (selectedLanguage) {
-        case 'en':
+        case 'en':      
 
-            let enContent;
-        
             for (let i = 0; i < paragraph.length; i++) {
                 paragraph[i].innerText = enText[i];
             }
@@ -21,8 +19,6 @@ function changeTextLang(){
         break;
 
         case 'ru':
-        
-            let ruContent;
 
             for (let i = 0; i < paragraph.length; i++) {
                 paragraph[i].innerText = ruText[i];
@@ -43,31 +39,12 @@ let divForVideo = document.getElementsByClassName('video')[0];
 playBtn.onclick= () => {
     let video = document.createElement('video');
         video.setAttribute('id', 'myVideo');
+        video.setAttribute('controls', 'true');
         video.setAttribute('src', 'videos/All over in 10 seconds.mp4');
     if (divForVideo.childNodes.length==0) 
     {
         divForVideo.appendChild(video);  
         video.play();
+        playBtn.style.display='none';
     }
-    else
-    {
-        video = document.getElementById('myVideo');
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
-    }
-    
-    video.addEventListener('play',() => {
-        playBtn.innerText = "Pause video";
-    }, false);
-             
-    video.addEventListener('pause',() => {
-        playBtn.innerText = "Play video";
-    }, false);
-
-    video.addEventListener('ended', () => {
-        playBtn.innerText = "Replay video";
-    }, false);    
 }
